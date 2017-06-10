da5f6a805b847e504f2a2dbb823790976e5e9b39
ChadBailey <chadbailey904@gmail.com>
2017-06-10 17:58:45 -0500

    Added comments

--- media.py
+++ media.py
@@ -1,13 +1,30 @@
+#Import webbrowser for opening trailer
 import webbrowser
 
 class Movie():
+	"""
+	Movie class
+	Used to store attributes of movies to then be processed into an html file using fresh_tomatoes.py
+	
+	Input:
+		Movie title, storyline, image url, trailer url, star rating (integer 0-5), release date
+	
+	Output:
+		None
+	
+	Methods:
+		show_trailer():
+			opens youtube trailer URL in a browser
+	"""
+
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, star_rating, release_date):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		self.star_rating = star_rating
-		self.release_date = release_date
-		if not 0 <= self.star_rating <= 5: raise Exception('Star rating must be between 0 and 5.')
+		self.title = movie_title #Movie title (string)
+		self.storyline = movie_storyline #Movie storyline (string)
+		self.poster_image_url = poster_image #Poster image (string)
+		self.trailer_youtube_url = trailer_youtube #Youtube trailer url (string)
+		self.star_rating = star_rating #Star rating 0-5 (int)
+		self.release_date = release_date #Release date (string)
+		if not 0 <= self.star_rating <= 5: raise Exception('Star rating must be between 0 and 5.') #Prevent bad input
 	def show_trailer(self):
+		#Open trailer url
 		webbrowser.open(self.trailer_youtube_url)
